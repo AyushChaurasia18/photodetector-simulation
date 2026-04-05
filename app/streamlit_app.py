efc9f7c5d1ada49cb4ba883a26cf87516590a6ec
Ayush Chaurasia <ayush.chaurasia19@gmail.com>
2026-04-05 21:03:06 +0530

    fixed import error

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,5 +1,6 @@
 # app/streamlit_app.py
-
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import streamlit as st
 import numpy as np
 from PIL import Image
@@ -9,8 +10,7 @@ from config import DEFAULT_CONFIG
 from utils.metrics import mse, psnr, snr
 from utils.image_generator import generate_default_image
 
-import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 st.set_page_config(layout="wide")
 
 st.title("📷 Photon-Limited Imaging Simulator")
